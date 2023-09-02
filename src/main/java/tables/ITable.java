f13fd719f4c662ea358b19324f7a3084610b3e4b
Татьяна Сердюк <131401889+TatyanaSerdyuk@users.noreply.github.com>
2023-09-02 21:11:41 +0700

    ITable.java

--- src/main/java/tables/ITable.java
+++ src/main/java/tables/ITable.java
@@ -1,2 +1,18 @@
-package tables;public class ITable {
+package tables;
+
+import java.sql.SQLException;
+
+public interface ITable {
+    void create() throws SQLException;
+    void delete() throws SQLException;
+    void insert() throws SQLException;
+    /*void select() throws SQLException;*/
+
+    void update() throws SQLException;
+
+    void selectFemaleStudents() throws SQLException;
+
+    void getSelectCount() throws SQLException;
+
+    void getStudentsByNameGroup() throws SQLException;
 }
