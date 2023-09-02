a21dfee56edfa9b7a29e119132668f8067d55e1a
Татьяна Сердюк <131401889+TatyanaSerdyuk@users.noreply.github.com>
2023-09-02 21:13:45 +0700

    PropertiesReader.java

--- src/main/java/utils/PropertiesReader.java
+++ src/main/java/utils/PropertiesReader.java
@@ -1,2 +1,34 @@
-package utils;public class PropertiesReader {
+package utils;
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+
+public class PropertiesReader implements utils.resources.IResource {
+
+    @Override
+    public Map<String, String> read() {
+        String rootFolder = System.getProperty("user.dir");
+
+        Map<String, String> props = new HashMap<>();
+
+        try (InputStream input = new FileInputStream(String.format("%s/src/main/resources/%s", rootFolder, "db.properties"))) {
+            Properties prop = new Properties();
+            prop.load(input);
+
+            for (Object key : prop.keySet()) {
+                props.put((String)key, prop.getProperty((String)key));
+            }
+            return props;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
