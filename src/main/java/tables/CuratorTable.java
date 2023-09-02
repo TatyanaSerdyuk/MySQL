60747582b6dfb7ff9472183e9bc2a903b4b5ecd3
Татьяна Сердюк <131401889+TatyanaSerdyuk@users.noreply.github.com>
2023-09-02 21:10:43 +0700

    CuratorTable.java

--- src/main/java/tables/CuratorTable.java
+++ src/main/java/tables/CuratorTable.java
@@ -1,2 +1,25 @@
-package tables;public class CuratorTable {
+package tables;
+
+import java.sql.SQLException;
+
+public class CuratorTable extends AbsTable{
+    private String tableName = "curators";
+
+    public CuratorTable() {super("curators"); }
+
+
+    @Override
+    public void create() throws SQLException {
+        iDbExecutor.execute(String.format("CREATE table %s (id int not null, fio varchar(40));", tableName), false);
+
+    }
+
+    @Override
+    public void insert() throws SQLException {
+        iDbExecutor.execute(String.format("INSERT INTO %s VALUES " +
+                "(1, 'Ракова Агата'), " +
+                "(2, 'Трофимов Герман'), " +
+                "(3, 'Хомяков Роберт'), " +
+                "(4, 'Козлов Тимур');", tableName), false);
+    }
 }
